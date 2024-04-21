f5c08959822f40ea811b4b7969c8709f6c8453e5
Nam Huynh <124078144+nsh1507@users.noreply.github.com>
2024-04-21 03:44:28 -0400

    Delete src directory

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module BFSPuzzleSolver {
-    requires transitive javafx.controls;
-    exports puzzles.common;
-    exports puzzles.common.solver;
-    exports puzzles.hoppers.gui;
-    exports puzzles.hoppers.model;
-    exports puzzles.chess.gui;
-    exports puzzles.chess.model;
-}
